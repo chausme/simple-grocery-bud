f5da9a611a9b95fc26edc5a873526212e79ff883
Alex Chaus <chausme@gmail.com>
2022-12-16 16:35:25 +0400

    Add logic Round 10

--- src/groceryBud.js
+++ src/groceryBud.js
@@ -9,11 +9,19 @@ class GroceryBud {
 
     #info = this.#infoWrap.querySelector('.info');
 
+    #list = document.querySelector('.grocery-list');
+
+    #container = document.querySelector('.grocery-container');
+
     #editId;
 
     // init
 
     init = () => {
+        // load items
+
+        window.addEventListener('DOMContentLoaded', this.#outputItems);
+
         // process form submission
 
         this.#form.addEventListener('submit', e => {
@@ -24,38 +32,20 @@ class GroceryBud {
             const { action } = e.target.dataset;
             const grocery = this.#getGrocery();
 
-            const list = document.querySelector('.grocery-list');
-
             if (grocery && action === 'add') {
-                const element = document.createElement('article');
                 // generate id based on timestamp
                 const uuid = new Date().getTime().toString();
-                const container = document.querySelector('.grocery-container');
-                element.classList.add('grocery-item');
-                element.dataset.id = uuid;
-                element.innerHTML = `
-                    <p class="title">${grocery}</p>
-                    <div class="actions">
-                        <button type="button" class="btn edit p-0">
-                            <i class="fas fa-edit"></i>
-                        </button>
-                        <button type="button" class="btn delete p-0 ms-3">
-                            <i class="fas fa-trash"></i>
-                        </button>
-                    </div>
-                `;
-                const editBtn = element.querySelector('.edit');
-                const deleteBtn = element.querySelector('.delete');
-                editBtn.addEventListener('click', this.#editItem);
-                deleteBtn.addEventListener('click', this.#deleteItem);
-                list.appendChild(element);
-                container.classList.add('visible');
+
+                this.#appendListItem(uuid, grocery);
+
                 this.#addToLocalStorage(uuid, grocery);
                 this.#setDefaultState();
+
                 this.#outputInfo('Succesfully added grocery item', 'success');
             } else if (grocery && action === 'edit') {
-                list.querySelector(`.grocery-item[data-id="${this.#editId}"] .title`).textContent =
-                    this.#getGrocery();
+                this.#list.querySelector(
+                    `.grocery-item[data-id="${this.#editId}"] .title`
+                ).textContent = this.#getGrocery();
                 this.#updateInLocalStorage(this.#editId);
                 this.#setDefaultState();
                 this.#outputInfo('Succesfully updated grocery item', 'success');
@@ -139,6 +129,44 @@ class GroceryBud {
 
     static #getLocalStorage = () => JSON.parse(localStorage.getItem('list'));
 
+    // create and append list item
+
+    #appendListItem = (id, name) => {
+        const element = document.createElement('article');
+        element.classList.add('grocery-item');
+        element.dataset.id = id;
+        element.innerHTML = `
+            <p class="title">${name}</p>
+            <div class="actions">
+                <button type="button" class="btn edit p-0">
+                    <i class="fas fa-edit"></i>
+                </button>
+                <button type="button" class="btn delete p-0 ms-3">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </div>
+        `;
+        const editBtn = element.querySelector('.edit');
+        const deleteBtn = element.querySelector('.delete');
+        editBtn.addEventListener('click', this.#editItem);
+        deleteBtn.addEventListener('click', this.#deleteItem);
+        this.#list.appendChild(element);
+        this.#container.classList.add('visible');
+    };
+
+    // output items
+
+    #outputItems = () => {
+        const items = this.constructor.#getLocalStorage();
+        if (!items || !items.length) {
+            return;
+        }
+        items.forEach(item => {
+            console.log(item);
+            this.#appendListItem(item.id, item.name);
+        });
+    };
+
     // edit item
 
     #editItem = e => {
@@ -161,9 +189,13 @@ class GroceryBud {
         const itemEl = e.target.closest('.grocery-item');
         const itemId = itemEl.dataset.id;
         itemEl.remove();
-
-        // check for list length and hide the list if there are no items
         this.#removeFromLocalStorage(itemId);
+
+        const items = this.constructor.#getLocalStorage();
+        if (!items.length) {
+            this.#container.classList.remove('visible');
+        }
+
         this.#outputInfo(`Item ${itemId} has been removed`, 'success');
         this.#setDefaultState();
     };
@@ -171,15 +203,13 @@ class GroceryBud {
     // remove items
 
     #clearItems = () => {
-        const container = document.querySelector('.grocery-container');
-        const list = document.querySelector('.grocery-list');
         const items = document.querySelectorAll('.grocery-item');
         if (items.length) {
             items.forEach(item => {
-                list.removeChild(item);
+                this.#list.removeChild(item);
             });
         }
-        container.classList.remove('visible');
+        this.#container.classList.remove('visible');
         this.#outputInfo('Heads up, your list is empty now', 'error');
         this.#setDefaultState();
         localStorage.removeItem('list');
